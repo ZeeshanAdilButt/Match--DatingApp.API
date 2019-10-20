7648de2ef45c30ea34c5517df49b9664e5853b00
Zeeshan <zeeabutt@outlook.com>
2019-10-21 02:01:24 +0500

    added CORS

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -31,6 +31,8 @@ namespace DatingApp.API
             x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +49,7 @@ namespace DatingApp.API
             }
 
             app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
